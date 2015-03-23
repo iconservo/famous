b22e240c8afad069c4ccc5c9454680e0b9c0b8dd
Hongxu Liu <djshu.us@gmail.com>
2015-03-23 18:17:13 -0500

    Added composer event methods

--- src/mvc/Composer.js
+++ src/mvc/Composer.js
@@ -8,5 +8,20 @@ define(function(require, exports, module) {
         EventHandler.setOutputHandler(this, this._eventOutput);
     }
 
+    Composer.prototype.registerEvents = function(view, event, handler) {
+        if (typeof event === 'string') view.on('event', handler);
+        else {
+            for (var key in event) {
+                view.on(key, event[key]);
+            }
+        }
+    };
+
+    Composer.prototype..reEmit = function(view, event, newEvent) {
+        view.on(event, function(arg) {
+            this._eventOutput.emit(newEvent || event, arg);
+        }.bind(this));
+    };
+
     module.exports = Composer;
 });
