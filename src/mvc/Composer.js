48cd060e4e80a78764e3013525d2aa1f894d9979
Hongxu Liu <djshu.us@gmail.com>
2015-03-24 20:54:37 -0700

    updated composer

--- src/mvc/Composer.js
+++ src/mvc/Composer.js
@@ -8,17 +8,26 @@ define(function(require, exports, module) {
         EventHandler.setOutputHandler(this, this._eventOutput);
     }
 
-    Composer.prototype.registerEvents = function(view, event, handler) {
-        if (typeof event === 'string') view.on('event', handler);
-        else {
-            for (var key in event) {
-                view.on(key, event[key]);
-            }
+    Composer.prototype.registerViewsActions = function(actionEvents) {
+        for (var i = 0; i < actionEvents.length; i++) {
+            var action = actionEvents[i];
+            var view = action[0];
+            var event = action[1];
+            var handler = action[2] || this.viewsActions[event];
+            view.on(event, handler);
         }
     };
 
-    Composer.prototype.reEmit = function(view, event, newEvent) {
-        view.on(event, function(arg) {
+    Composer.prototype.registerControllerActions = function(actionEvents, controller) {
+        for (var i = 0; i < actionEvents.length; i++) {
+            var event = actionEvents[i];
+            var handler = this.controllerActions[event];
+            controller.on(event, handler);
+        }
+    };
+
+    Composer.prototype.relay = function(emitter, event, newEvent) {
+        emitter.on(event, function(arg) {
             this._eventOutput.emit(newEvent || event, arg);
         }.bind(this));
     };
