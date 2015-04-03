90d844fcb74d609670fb1b6dc57bc061ee34b7e5
Hongxu Liu <djshu.us@gmail.com>
2015-04-03 07:31:14 -0700

    Updated Element

--- src/core/Element.js
+++ src/core/Element.js
@@ -3,19 +3,26 @@ define(function(require, exports, module) {
 
     function Element(options) {
         this.target = options.target;
-        if (options.position) this.translate = Transform.translate.apply(null, options.position);
-        if (options.origin) this.origin = options.origin;
+        this.size = options.size;
+        if (options.translate) this.transform = Transform.translate.apply(null, options.translate);
+        this.origin = options.origin;
     }
 
     Element.prototype.render = function() {
-        if (!this.target) return;
+        var size = this.size;
+        if (!size && this.target.getSize) size = this.target.getSize();
+
         return {
             target: this.target.render(),
-            size: this.target.getSize && this.target.getSize(),
-            transform: this.translate && this.translate,
-            origin: this.origin && this.origin,
-            align: this.align && this.origin
+            size: size,
+            transform: this.transform,
+            origin: this.origin,
+            align: this.origin
         };
+    };
+
+    Element.prototype.getSize = function() {
+        return this.target.getSize();
     };
 
     module.exports = Element;
